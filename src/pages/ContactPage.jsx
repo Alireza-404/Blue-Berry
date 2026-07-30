cb620bb39c12d06acbadcfe9eccbe64bef4cd9e0
Alireza Shabani <alireza4o4shabani@gmail.com>
2026-07-30 17:23:16 +0330

    Update => Contact Page Form Validation

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,11 +5,19 @@ import SecondaryButton from "../components/Ui/SecondaryButton/SecondaryButton";
 import gsap from "gsap";
 
 import { Trans, useTranslation } from "react-i18next";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export default function ContactPage() {
   const { t } = useTranslation();
   const containerRef = useRef(null);
+
+  const [value, setValue] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    comment: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
 
   useLayoutEffect(() => {
     const ctx = gsap.context((self) => {
@@ -37,6 +45,62 @@ export default function ContactPage() {
 
     return () => ctx.revert();
   }, []);
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+
+    setValue((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const { firstname, lastname, email, comment } = value;
+
+    if (!firstname) {
+      setErrorMessage(t("contactPage.messages.firstNameRequired"));
+      return;
+    }
+
+    if (firstname.length < 3 || firstname.length > 40) {
+      setErrorMessage(t("contactPage.messages.invalidFirstName"));
+      return;
+    }
+
+    if (!lastname) {
+      setErrorMessage(t("contactPage.messages.lastNameRequired"));
+      return;
+    }
+
+    if (lastname.length < 3 || lastname.length > 40) {
+      setErrorMessage(t("contactPage.messages.invalidLastName"));
+      return;
+    }
+
+    if (!email) {
+      setErrorMessage(t("contactPage.messages.emailRequired"));
+      return;
+    }
+
+    const emailRegex = /^[\w.+-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+      setErrorMessage(t("contactPage.messages.invalidEmail"));
+      return;
+    }
+
+    if (!comment) {
+      setErrorMessage(t("contactPage.messages.messageRequired"));
+      return;
+    }
+
+    if (comment.length < 10) {
+      setErrorMessage(t("contactPage.messages.invalidMessage"));
+      return;
+    }
+
+    setErrorMessage("");
+  };
+
   return (
     <>
       <header>
@@ -76,54 +140,67 @@ export default function ContactPage() {
             className="p-8 border border-TB/15 dark:border-box-border-D
             rounded-3xl flex flex-col gap-y-6 w-full lg:w-[650px] duration-0"
             id="contact-form"
+            onSubmit={onSubmitHandler}
           >
             <input
               id="firstname"
               type="text"
               name="firstname"
+              value={value.firstname}
+              onChange={onChangeHandler}
               spellCheck={false}
               placeholder={t("contactPage.firstName")}
               className="py-3 px-3.5 border border-TB/15 dark:border-box-border-D
               w-full focus:border-TB/40 outline-none rounded-lg dark:bg-box-D
               dark:focus:border-[#5a606f] placeholder:select-none text-TB dark:text-white"
-              required
             />
 
             <input
               id="lastname"
               type="text"
               name="lastname"
+              value={value.lastname}
+              onChange={onChangeHandler}
               spellCheck={false}
               placeholder={t("contactPage.lastName")}
               className="py-3 px-3.5 border border-TB/15 dark:border-box-border-D
               w-full focus:border-TB/40 outline-none rounded-lg dark:bg-box-D
               dark:focus:border-[#5a606f] placeholder:select-none text-TB dark:text-white"
-              required
             />
 
             <input
               id="email"
-              type="email"
+              type="text"
               name="email"
               autoComplete="email"
+              value={value.email}
+              onChange={onChangeHandler}
               spellCheck={false}
               placeholder={t("contactPage.email")}
               className="py-3 px-3.5 border border-TB/15 dark:border-box-border-D
               w-full focus:border-TB/40 outline-none rounded-lg dark:bg-box-D
               dark:focus:border-[#5a606f] placeholder:select-none text-TB dark:text-white"
-              required
+              formNoValidate
             />
 
             <textarea
               name="comment"
               id="comment"
+              value={value.comment}
+              onChange={onChangeHandler}
               placeholder={t("contactPage.comment")}
               className="py-3 px-3.5 border border-TB/15 dark:border-box-border-D
               w-full focus:border-TB/40 outline-none rounded-lg dark:bg-box-D resize-none
-              dark:focus:border-[#5a606f] placeholder:select-none text-TB dark:text-white"
+              dark:focus:border-[#5a606f] placeholder:select-none text-TB dark:text-white
+              my-scroll"
               rows={7}
-              required
             ></textarea>
+
+            {errorMessage && (
+              <p className="text-red-500 font-bold text-lg text-center">
+                {errorMessage}
+              </p>
+            )}
 
             <SecondaryButton
               type={"submit"}
